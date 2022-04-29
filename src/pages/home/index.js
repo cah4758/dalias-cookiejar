6d61175037af228c6c80be94c173cd44a7b4c940
Charles Hernandez <charlesh4758@gmail.com>
2022-04-29 15:21:19 -0500

    Homepage population, favicon addition

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Photos from "../../components/carousel";
+import Intro from "../../components/intro";
 
 function Home() {
-  return <p>Hi there!</p>;
+  return (
+    <Container className="py-3 px-0">
+      <Row>
+        <Col xs={12} md={7}>
+          <Intro />
+        </Col>
+        <Col xs={12} md={5}>
+          <Photos />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
